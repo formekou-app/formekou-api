88eb1a99e1c67576443c5c337456abb03b0cd345
RickaPrincy <rckprincy@gmail.com>
2024-02-02 17:12:35 +0300

    chore: init project

---
+++ src/main/java/neocode/formekouapi/FormekouApiApplication.java
@@ -0,0 +1,13 @@
+package neocode.formekouapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormekouApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormekouApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neocode/formekouapi/FormekouApiApplicationTests.java
@@ -0,0 +1,13 @@
+package neocode.formekouapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormekouApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
