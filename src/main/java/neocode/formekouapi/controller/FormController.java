7f7292ef8887850cc1ebb98e88cea74f31f20ea6
tsanta-niaina-MT <hei.tsantaniaina@gmail.com>
2024-02-07 10:41:21 +0100

    chore: FormController

--- src/main/java/neocode/formekouapi/controller/FormController.java
+++ src/main/java/neocode/formekouapi/controller/FormController.java
@@ -1,2 +1,34 @@
-package neocode.formekouapi.controller;public class FormController {
+package neocode.formekouapi.controller;
+
+import neocode.formekouapi.model.Form;
+import neocode.formekouapi.service.FormService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/forms")
+public class FormController {
+    private final FormService formService = new FormService();
+
+    @GetMapping
+    public List<Form> getallForms() {
+        return formService.getAllForms();
+    }
+
+    @GetMapping
+    public Optional<Form> getFormsById(String id) {
+        return formService.getFormsById();
+    }
+
+    @PutMapping
+    public Form createOrUpdateForm(@RequestBody Form formData) {
+        return formService.createOrUpdate(formData);
+    }
+
+    @DeleteMapping
+    public void deleteFormById(String id) {
+        formService.deleteFormById(id);
+    }
 }
